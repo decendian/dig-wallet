b66dd390bfae4a273285f4915484e92844ee045f
Kia Karimi <66522497+Kia82@users.noreply.github.com>
2025-03-17 07:55:18 -0700

    added/implemented hashing funciton and scaffolding for a Block

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use blockchainlib::*;
 fn main() {
-    println!("Hello, world!");
+    let block = Block::new(0,0,vec![0;16],0,"Debug".to_string());
+    println!("Un-Hashed Block: {:?}",block);
+    //Doesn't have Display trait implemented as it's a Vec<u8>,
+    // therefor we use debug output {:?}
+    println!("Block Hash: {:?}",block.hash());
 }
