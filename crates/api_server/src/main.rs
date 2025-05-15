90d44d44b157c2a95c7f7dc52b9d0c653df612f5
KiaK <kiak01@students.cs.ubc.ca>
2025-05-15 16:29:54 -0700

    Refactor DID registry path to use environment variable
    Replaced hardcoded DID registry path with an environment variable for better configurability. Updated `.env` file and related code to reflect this change. Also added TODOs for user input handling and improved `CreateDIDRequest` structure for future implementation.

--- crates/api_server/src/main.rs
+++ crates/api_server/src/main.rs
@@ -1,7 +1,6 @@
 use actix_cors::Cors;
 use actix_web::{middleware, web, App, HttpResponse, HttpServer, Responder};
 use config::{Config, ConfigError, Environment, File};
-use did_library::did::core::key_utils::KeyType;
 use did_library::did::core::{did_document::DIDCreationOptions, traits::DIDMethod};
 use did_library::did::methods::key::handler::KeyDID;
 use did_library::DIDDocument;
@@ -65,7 +64,10 @@ struct IssueCredentialRequest {
 }
 
 #[derive(Deserialize)]
-struct CreateDIDRequest {}
+//TODO: Implement this
+struct CreateDIDRequest {
+    
+}
 
 #[derive(Serialize, Deserialize)]
 pub struct CreateDIDResponse {
@@ -73,11 +75,11 @@ pub struct CreateDIDResponse {
 }
 
 /// Handler for creating a new DID
-/// This endpoint generates a new decentralized identifier using KeyDID method
+/// TODO: Make so that it can handle/manage input from a user
 async fn create_did_handler(req: web::Json<CreateDIDRequest>) -> impl Responder {
 
     // Path to the registry file
-    let registry_path = "../did_library/resources/did_registry.json";
+    let registry_path = env::var("DID_REGISTRY_PATH").unwrap();
 
     // Check if the registry file exists and has content
     if let Ok(metadata) = std::fs::metadata(registry_path) {
@@ -105,12 +107,7 @@ async fn create_did_handler(req: web::Json<CreateDIDRequest>) -> impl Responder 
         service: None,
     };
 
-    //TODO: Dynamically load and configure path for registry initialization (from .env file).
-    // Alireza if you see this, you are going to laugh at me for not doing this.
-    did_library::did::registry::init_registry(Some(
-        "../did_library/resources/did_registry.json".to_string(),
-    ));
-
+    did_library::did::registry::init_registry(Some(env::var("DID_REGISTRY_PATH").unwrap()));
     let document = did_method.create_did(options);
 
     // Return the DID document
